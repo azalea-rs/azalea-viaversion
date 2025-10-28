9740430510dfe3477bdce2bd68d51ba8b96ee023
Shayne Hartford <shaybox@shaybox.com>
2025-10-28 14:55:34 -0400

    Update ViaProxy for 1.21.10

--- src/lib.rs
+++ src/lib.rs
@@ -32,7 +32,7 @@ use tracing::{error, trace, warn};
 const JAVA_DOWNLOAD_URL: &str = "https://adoptium.net/installation";
 const VIA_OAUTH_VERSION: Version = Version::new(1, 0, 2);
 // https://github.com/ViaVersion/ViaProxy/releases
-const VIA_PROXY_VERSION: Version = Version::new(3, 4, 5);
+const VIA_PROXY_VERSION: Version = Version::new(3, 4, 6);
 
 #[derive(Clone, Resource)]
 pub struct ViaVersionPlugin {
